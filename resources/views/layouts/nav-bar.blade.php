fcb3f16c79ff3a68803dfb5b9a39e2a063b23b85
unknown <Adi Sumardi>
2022-12-07 12:07:12 +0700

    first commit

---
+++ resources/views/layouts/nav-bar.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <ul class="nav">
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Barang</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Distributor</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link">Jasa Pengiriman</a>
+                    </li>
+                  </ul>
+            </div>
+        </div>
+    </div>
+</div>
